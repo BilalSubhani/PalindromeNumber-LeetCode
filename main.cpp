8a0fe9d3b21eb2625ffb874bd1f99d170aafc91c
Bilal <139504369+BilalSubhani@users.noreply.github.com>
2024-06-07 03:15:57 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0)
+        if(x < 0 || (x % 10 == 0 && x != 0))
             return false;
-        else{
-            int num = x;
-            
-            int reversed = 0;
-            while (num) {
-                reversed = reversed * 10 + num % 10;
-                num /= 10;
-            }
-
-            return x == reversed;
+        
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
+
+        return x == reversed || x == reversed / 10;
     }
 };
